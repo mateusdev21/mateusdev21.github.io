ecad3201daf5f44cd3ad39ef151cea154238a8db
frontend-2 <mateus.dev21@gmail.com>
2021-10-21 15:17:28 +0700

    adding assets

---
+++ src/pages/home.php
@@ -0,0 +1,11 @@
+<div class="container container-fluid">
+    <div class="jumbotron">
+        <p style="font-size: 1.5rem;">Hello My Name Is</p>
+        <h1>MATEUS<br>ARGASTRA</h1>
+        <div class="d-flex flex-row">
+            <p>And I'm a</p>
+            <p style="color: orangered;">&nbsp;Frontend Developer</p>
+        </div>
+        <a class="btn btn-outline-primary" href="#" role="button">Contact Me</a>
+    </div>
+</div>
---
+++ src/pages/template/navbar.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-expand-lg navbar-dark">
+    <a class="navbar-brand" href="#">
+        <div class="d-flex flex-row">
+            <p style="color:white">Mateus</p>
+            <p style="color:orangered;">&nbsp;Argastra&nbsp;.</p>
+        </div>
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse d-flex flex-row-reverse" id="navbarNav">
+        <div class="navbar-nav">
+            <a class="nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="#">About</a>
+            <a class="nav-link" href="#">Skills</a>
+            <a class="nav-link" href="#">Contact</a>
+        </div>
+    </div>
+</nav>
