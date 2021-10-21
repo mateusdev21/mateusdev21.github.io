cca40582cc11c3622bf827a16b4b41198faa676e
Mateus Argastra Manikotama Riyanto <69667901+mateusdev21@users.noreply.github.com>
2021-10-21 15:12:26 +0700

    First Init

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php include "src/pages/template/header.php"; ?>
+<?php include "src/pages/template/navbar.php"; ?>
+<?php include "src/pages/home.php"; ?>
+<?php include "src/pages/template/footer.php"; ?>
+    
